d1a25757d920fb7a717aec215a5c837c9f39e428
break-jahun <jh.lee@break.co.kr>
2023-05-24 16:28:12 +0900

    first commit

---
+++ App.tsx
@@ -0,0 +1,19 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function App(): JSX.Element {
+  return (
+    <View>
+      <Text>BREAK RN</Text>
+    </View>
+  );
+}
+
+export default App;
