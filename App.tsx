688ea1504b26c6dd2835c42d7e23181782e7c645
break-jahun <jh.lee@break.co.kr>
2023-05-26 10:20:24 +0900

    test

--- App.tsx
+++ App.tsx
@@ -7,21 +7,35 @@
 
 import React, {useRef} from 'react';
 import {WebView} from 'react-native-webview';
+import {SafeAreaView, StyleSheet, Share} from 'react-native';
 
 function App(): JSX.Element {
   const webviewRef = useRef(null);
+
+  const handleShare = async () => {
+    await Share.share({
+      url: 'https://market.break.co.kr',
+      title: '브레이크',
+      message: 'https://market.break.co.kr',
+    });
+  };
+
   return (
-    <WebView
-      ref={webviewRef}
-      // style={styles.container}
-      // source={{uri: 'https://market.break.co.kr'}}
-      source={{uri: 'http://192.168.0.93:3000/test'}}
-      onMessage={event => {
-        console.log('event.nativeEvent.data', event.nativeEvent.data);
-        (webviewRef.current as any).postMessage('answer...');
-      }}
-    />
+    <SafeAreaView style={styles.container}>
+      <WebView
+        ref={webviewRef}
+        // source={{uri: 'https://market.break.co.kr'}}
+        source={{uri: 'http://192.168.0.93:3000/test'}}
+        onMessage={handleShare}
+      />
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
 
 export default App;
